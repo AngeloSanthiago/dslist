f5da7292cf32ed68a66ff4f2eacf832f3fef7f6c
AngeloSanthiago <angelosanthiago@gmail.com>
2025-05-20 21:34:09 -0300

    Aula01-Compelto Game Repository Service Controller

--- dslist/src/main/java/com/devsuperior/dslist/dto/GameMinDTO.java
+++ dslist/src/main/java/com/devsuperior/dslist/dto/GameMinDTO.java
@@ -15,10 +15,29 @@ public class GameMinDTO {
     public GameMinDTO(Game entity) {
         this.id = entity.getId();
         this.title = entity.getTitle();
-        this.year = year;
-        this.imgUrl = imgUrl;
-        this.shortDescription = shortDescription;
+        this.year = entity.getYear();
+        this.imgUrl = entity.getImgUrl();
+        this.shortDescription = entity.getShortDescription();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public String getShortDescription() {
+        return shortDescription;
     }
     
-
 }
